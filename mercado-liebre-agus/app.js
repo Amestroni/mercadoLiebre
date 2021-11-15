e5ea3684b3b2b16a95958d1f02eb40763dece9b5
Amestroni <agustinmestroni@hotmail.com>
2021-11-14 21:15:19 -0300

    configurando puerto para iniciar deploy

--- mercado-liebre-agus/app.js
+++ mercado-liebre-agus/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use (express.static(path.resolve(__dirname, './public')));
 
-app.listen(3000, () => console.log('servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('servidor corriendo en el puerto 3000'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
